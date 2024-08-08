65bc5ad65194fb798a57d138b044fc0d100ef339
AsiKlara <klara.anna@sindler.cz>
2024-08-09 00:36:02 +0200

    baf

--- app.py
+++ app.py
@@ -6,21 +6,6 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        # Get form data
-        n_pendulums = int(request.form['n_pendulums'])
-        d_diff = float(request.form['d_diff'])
-        t_max = float(request.form['t_max'])
-        g = float(request.form['g'])
-        m1 = float(request.form['m1'])
-        m2 = float(request.form['m2'])
-        L1 = float(request.form['L1'])
-        L2 = float(request.form['L2'])
-        theta1 = float(request.form['theta1'])
-        theta2 = float(request.form['theta2'])
-        colormap = request.form['colormap']
-        background = request.form['background']
-
     return render_template('index.html')
 
 
