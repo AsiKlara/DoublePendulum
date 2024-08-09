44830d47dbf9a3e86876f4ce4a99eed6c99ebd88
AsiKlara <klara.anna@sindler.cz>
2024-08-09 18:45:30 +0200

    lot of things (ipfs+nftmint on assethub)

---
+++ api.py
@@ -0,0 +1,172 @@
+import requests
+import json
+import time
+import os
+
+bucketuuid = os.getenv("BUCKETUUID")
+authentication = os.getenv("AUTHENTICATION")
+
+# The URL of the REST API endpoint
+url_upload = f'https://api.apillon.io/storage/buckets/{bucketuuid}/upload'
+
+
+def post_ipfs():
+    name = "double_pendulum_animation.mp4"
+
+    # The path to the video file you want to upload
+    file_path = f'./{name}'
+
+    # Open the file in binary mode and prepare it for upload
+    with open(file_path, 'rb') as file:
+        headers_upload = {
+            "Authorization": authentication,
+            "Content-Type": "application/json"
+        }
+
+        # Define the JSON data payload
+        data = {
+            "files": [
+                {
+                    "fileName": name,
+                    "contentType": "video/mp4"
+                }
+            ]
+        }
+
+        # Convert the data payload to a JSON string
+        json_data = json.dumps(data)
+
+        # Send the POST request with the file
+        response = requests.post(url_upload, headers=headers_upload, data=json_data)
+
+    # to dict
+    data_dict = json.loads(response.text)
+
+    # Define the endpoint URL
+    url_up = data_dict["data"]["files"][0]["url"]
+
+    headers_up = {
+        "Content-Type": "video/mp4"
+    }
+
+    # Open the file in binary mode and send the PUT request
+    with open(file_path, 'rb') as file:
+        response = requests.put(url_up, headers=headers_up, data=file)
+
+        print(response)
+
+    url_end = f'https://api.apillon.io/storage/buckets/{bucketuuid}/upload/{data_dict["data"]["sessionUuid"]}/end'
+
+    # If there's any data to be sent with the request, define it here (for example, {})
+    # In this case, no data is specified, so we send an empty JSON object.
+    data = {}
+
+    # Send the POST request
+    response2 = requests.post(url_end, headers=headers_upload, json=data)
+
+    print(response2)
+
+    # Define the endpoint URL with bucket UUID and file UUID
+    url_file = f'https://api.apillon.io/storage/buckets/{bucketuuid}/files/{data_dict["data"]["files"][0]["fileUuid"]}'
+
+    # Headers for the GET request
+    headers = {
+        "Authorization": authentication  # Replace :credentials with actual base64-encoded credentials
+    }
+    cid = None
+    n = 0
+    while cid is None and n <= 5:
+        # Send the GET request
+        time.sleep(5)
+        response = requests.get(url_file, headers=headers)
+        data_dict2 = json.loads(response.text)
+        cid = data_dict2["data"]["CID"]
+        n += 1
+    print(f"first: {cid}")
+    return cid
+
+
+def post_json_ipfs(cid):
+    print(cid)
+    # create json metadata
+    metadata = """
+        {{
+          "name": "Double Pendulum",
+          "description": "",
+          "image": "ipfs://{0}",
+          "attributes": [
+            {{
+              "trait_type": "Color",
+              "value": "Blue"
+            }},
+            {{
+              "trait_type": "Size",
+              "value": "Large"
+            }}
+          ]
+        }}
+        """.format(cid)
+
+    metadata_json = json.dumps(metadata)
+
+    headers_upload2 = {
+        "Authorization": authentication,
+        "Content-Type": "application/json"
+    }
+
+    data = {
+        "files": [
+            {
+                "fileName": "metadata.json",
+                "contentType": "application/json"
+            }
+        ]
+    }
+
+    json_data = json.dumps(data)
+
+    # Send the POST request with the file
+    response = requests.post(url_upload, headers=headers_upload2, data=json_data)
+
+    data_dict2 = json.loads(response.text)
+
+    # Define the endpoint URL
+    url_up = data_dict2["data"]["files"][0]["url"]
+
+    headers_up = {
+        "Content-Type": "application/json"
+    }
+
+    # Open the file in binary mode and send the PUT request
+    response = requests.put(url_up, headers=headers_up, data=metadata_json)
+
+    url_end = f'https://api.apillon.io/storage/buckets/{bucketuuid}/upload/{data_dict2["data"]["sessionUuid"]}/end'
+
+    # If there's any data to be sent with the request, define it here (for example, {})
+    # In this case, no data is specified, so we send an empty JSON object.
+    data = {}
+
+    # Send the POST request
+    response2 = requests.post(url_end, headers=headers_upload2, json=data)
+
+    # Define the endpoint URL with bucket UUID and file UUID
+    url_file = f'https://api.apillon.io/storage/buckets/{bucketuuid}/files/{data_dict2["data"]["files"][0]["fileUuid"]}'
+
+    # Headers for the GET request
+    headers = {
+        "Authorization": authentication
+    }
+
+    cid2 = None
+    n = 0
+    while cid2 is None and n <= 5:
+        # Send the GET request
+        time.sleep(5)
+        response = requests.get(url_file, headers=headers)
+        data_dict2 = json.loads(response.text)
+        cid2 = data_dict2["data"]["CID"]
+        n += 1
+
+    # print(cid2)
+    return cid2
+
--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-
 from flask import Flask, render_template, request, send_from_directory
+from api import post_ipfs, post_json_ipfs
 from doublependulum import run_simulation
 
 app = Flask(__name__)
@@ -7,35 +7,14 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        # Get form data
-        n_pendulums = int(request.form['n_pendulums'])
-        d_diff = float(request.form['d_diff'])
-        t_max = float(request.form['t_max'])
-        g = float(request.form['g'])
-        m1 = float(request.form['m1'])
-        m2 = float(request.form['m2'])
-        L1 = float(request.form['L1'])
-        L2 = float(request.form['L2'])
-        theta1 = float(request.form['theta1'])
-        theta2 = float(request.form['theta2'])
+    return render_template('index.html', cid=None)
 
-        # Here you would run your simulation with the parameters
-        # For now, we'll just return them to show it works
-        return render_template('index.html', result={
-            'n_pendulums': n_pendulums,
-            'd_diff': d_diff,
-            't_max': t_max,
-            'g': g,
-            'm1': m1,
-            'm2': m2,
-            'L1': L1,
-            'L2': L2,
-            'theta1': theta1,
-            'theta2': theta2,
-        })
 
-    return render_template('index.html', result=None)
+@app.route("/mint_nft", methods=["POST"])
+def mint_nft():
+    cid = post_json_ipfs(post_ipfs())
+
+    return render_template("index.html", cid=cid)
 
 
 @app.route('/export', methods=['POST'])
@@ -50,8 +29,11 @@ def export():
     L2 = float(request.form['L2'])
     theta1 = float(request.form['theta1'])
     theta2 = float(request.form['theta2'])
+    colormap = request.form['colormap']
+    background = request.form['background']
 
-    run_simulation(n_pendulums, d_diff, t_max, g, m1, m2, L1, L2, theta1, theta2)
+    # Run the simulation with the provided parameters and colormap
+    run_simulation(n_pendulums, d_diff, t_max, g, m1, m2, L1, L2, theta1, theta2, colormap, background)
 
     return send_from_directory(".", 'double_pendulum_animation.mp4', as_attachment=True)
 
