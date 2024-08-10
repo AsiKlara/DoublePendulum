07536c9ef07f09af2566acda5ebb682082a87785
AsiKlara <klara.anna@sindler.cz>
2024-08-10 13:44:00 +0200

    requirements

--- celery_worker.py
+++ celery_worker.py
@@ -25,8 +25,8 @@ def make_celery(app):
 
 app = Flask(__name__)
 app.config.update(
-    CELERY_BROKER_URL='redis://localhost:6379/0',
-    CELERY_RESULT_BACKEND='redis://localhost:6379/0'
+    CELERY_BROKER_URL='redis://redis:6379/0',
+    CELERY_RESULT_BACKEND='redis://redis:6379/0'
 )
 
 celery = make_celery(app)
