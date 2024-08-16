0a519675b0bdb79ee41abab6c6030ae36dfe8150
AsiKlara <klara.anna@sindler.cz>
2024-08-16 10:55:19 +0200

    disable button

--- api.py
+++ api.py
@@ -38,7 +38,11 @@ def post_ipfs():
 
         # Send the POST request with the file
         response = requests.post(url_upload, headers=headers_upload, data=json_data)
-
+    print("ahoj")
+    print(response.text)
+    if not response.ok:
+        print("baf")
+        print(response.text)
     # to dict
     data_dict = json.loads(response.text)
 
